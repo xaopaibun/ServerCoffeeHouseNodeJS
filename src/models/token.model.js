bfac59129077eb5440e2829de95953b452cfeb5b
xaopaibun <vanquy33338888@gmail.com>
2021-12-26 12:57:16 +0700

    create model order

--- src/models/token.model.js
+++ src/models/token.model.js
@@ -36,9 +36,6 @@ const tokenSchema = mongoose.Schema(
 // add plugin that converts mongoose to json
 tokenSchema.plugin(toJSON);
 
-/**
- * @typedef Token
- */
 const Token = mongoose.model('Token', tokenSchema);
 
 module.exports = Token;
